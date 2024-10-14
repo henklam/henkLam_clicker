b65a45d9de825da85e6282d6b6bb37807f380323
henklam <183881683+henklam@users.noreply.github.com>
2024-10-14 14:06:55 -0400

    final

--- index.js
+++ index.js
@@ -4,7 +4,31 @@ document.head.appendChild(title);
 
 let count = 0;
 function increment() {
+    document.getElementById("error").innerHTML = "";
     count++;
     document.getElementById("count-el").innerHTML = count;
     console.log(count);
+}
+
+function decrement() {
+    if(count == 0) {
+        document.getElementById("error").innerHTML = "error: can't decrement";
+    } else {
+        count--;
+        document.getElementById("count-el").innerHTML = count;
+    }
+}
+
+function reset() {
+    document.getElementById("error").innerHTML = "";
+    document.getElementById("previous").innerHTML = "Previous entries:";
+    count = 0;
+    document.getElementById("count-el").innerHTML = count;
+}
+
+function save() {
+    document.getElementById("error").innerHTML = "";
+    document.getElementById("previous").innerHTML+= " " + count + " -";
+    count = 0;
+    document.getElementById("count-el").innerHTML = count;
 }
