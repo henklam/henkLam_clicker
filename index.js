775e9d68a8f9c6bfba3d66b86d1b98c625eb39e0
henklam <183881683+henklam@users.noreply.github.com>
2024-10-14 13:09:09 -0400

    added button that works

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 let title = document.createElement("title");
 title.innerHTML = "Clicker";
 document.head.appendChild(title);
+
+let count = 0;
+function increment() {
+    count++;
+    document.getElementById("count-el").innerHTML = count;
+    console.log(count);
+}
