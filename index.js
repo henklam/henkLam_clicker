1968b21388b531be8acbcda3b114585316838358
henklam <183881683+henklam@users.noreply.github.com>
2024-10-22 22:04:34 +0000

    did localStorage

--- index.js
+++ index.js
@@ -2,6 +2,21 @@ let title = document.createElement("title");
 title.innerHTML = "Clicker";
 document.head.appendChild(title);
 
+function printPrev() {
+    document.getElementById("previous").innerHTML = "Previous entries: ";
+    for(let i = 0; i < previous.length; i++) {
+        document.getElementById("previous").innerHTML+= " " + previous[i] + " -";
+    }
+}
+
+
+if(localStorage.getItem("previous") == null) {
+    var previous = [];
+} else {
+    var previous = JSON.parse(localStorage.getItem("previous"));
+    printPrev();
+}
+
 let count = 0;
 function increment() {
     document.getElementById("error").innerHTML = "";
@@ -24,11 +39,16 @@ function reset() {
     document.getElementById("previous").innerHTML = "Previous entries:";
     count = 0;
     document.getElementById("count-el").innerHTML = count;
+    previous = [];
+    localStorage.setItem("previous", JSON.stringify(previous));
 }
 
 function save() {
     document.getElementById("error").innerHTML = "";
-    document.getElementById("previous").innerHTML+= " " + count + " -";
+    // document.getElementById("previous").innerHTML+= " " + count + " -";
+    previous.push(count);
+    localStorage.setItem("previous", JSON.stringify(previous));
+    printPrev();
     count = 0;
     document.getElementById("count-el").innerHTML = count;
 }
